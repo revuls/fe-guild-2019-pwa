2f0f266457b70b3345b6e8b1c261dbdb006600db
Cesar Redondo Bueno <Cesar.Redondo.Bueno@ing.com>
2019-03-27 12:25:11 +0100

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ const manifest = {
             'sizes': '512x512'
         }
     ],
-    //"start_url": "index.html",
+    //'start_url': '/index.html',
     //"scope": ".",
     'display': 'standalone',
     'orientation': 'portrait-primary',
@@ -48,8 +48,7 @@ const manifest = {
     'dir': 'ltr',
     'lang': 'en-US'
 };
-let deferredPrompt;
-const enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
 
 window.addEventListener('load', () => {
     const base = document.querySelector('base');
@@ -72,132 +71,13 @@ window.addEventListener('load', () => {
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register(`${baseUrl}sw.js`)
-            .then(registration => {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            })
-            .catch(err => {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
+            .then( registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     }
 });
-
-window.addEventListener('beforeinstallprompt', event => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    event.preventDefault();
-
-    console.log('beforeinstallprompt fired');
-
-    // Stash the event so it can be triggered later.
-    deferredPrompt = event;
-
-    return false;
-});
-
-const displayConfirmNotification = () => {
-    if ('serviceWorker' in navigator) {
-        const options = {
-            body: 'You successfully subscribed to our Notification service!',
-            icon: 'src/images/icons/app-icon-96x96.png',
-            image: 'src/images/main-image-sm.jpg',
-            dir: 'ltr',
-            lang: 'en-US', // BCP 47,
-            vibrate: [100, 50, 200],
-            badge: 'src/images/icons/app-icon-96x96.png',
-            tag: 'confirm-notification',
-            renotify: true,
-            actions: [
-                {
-                    action: 'confirm',
-                    title: 'Okay',
-                    icon: 'src/images/icons/app-icon-96x96.png'
-                },
-                {
-                    action: 'cancel',
-                    title: 'Cancel',
-                    icon: 'src/images/icons/app-icon-96x96.png'
-                }
-            ]
-        };
-
-        navigator.serviceWorker.ready
-            .then(sw => sw.showNotification('Successfully subscribed (from SW)!', options));
-    }
-};
-
-const configurePushSubscription = () => {
-    if ('serviceWorker' in navigator) {
-        let serviceWorkerRegistration;
-        navigator.serviceWorker.ready
-            .then(registration => {
-                serviceWorkerRegistration = registration;
-                return registration.pushManager.getSubscription();
-            })
-            .then(subscription => {
-                if (subscription === null) {
-                    // Create a new subscription
-                    return serviceWorkerRegistration.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: urlBase64ToUint8Array(
-                            'BGMYWA_g_tpLeOXlR4oykccGE00remgS_-2PrH2WjgWrg93lPDsjnJ0pDKmuGaAfbzuOtvkUWK-CivusHQdL0BE'
-                        )
-                    });
-                }
-            })
-            .then(pushSubscription => {
-                return fetch(`${SERVER_URL}/subscriptions`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json'
-                    },
-                    body: JSON.stringify(pushSubscription)
-                });
-            })
-            .then(response => {
-                if (response.ok) {
-                    displayConfirmNotification();
-                }
-            })
-            .catch(error => console.log(error));
-    }
-};
-
-const unsubscribe = () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready
-            .then(serviceWorkerRegistration => {
-                return serviceWorkerRegistration.pushManager.getSubscription();
-            })
-            .then(subscription => {
-                if (!subscription) {
-                    console.log("Not subscribed, nothing to do.");
-                    return;
-                }
-                return subscription.unsubscribe();
-            })
-            .then(() => console.log("Successfully unsubscribed!."))
-            .catch(error => console.error('Error thrown while unsubscribing from push messaging', error));
-    }
-};
-
-const askForNotificationPermission = () => {
-    Notification.requestPermission(result => {
-        console.log('User Choice', result);
-        if (result !== 'granted') {
-            console.log('No notification permission granted!');
-        } else {
-            console.log('Notification permission granted!');
-            // displayConfirmNotification();
-            configurePushSubscription();
-        }
-    });
-};
-
-if ('Notification' in window) {
-    for (let i = 0; i < enableNotificationsButtons.length; i++) {
-        enableNotificationsButtons[i].style.display = 'inline-block';
-        enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
-    }
-}
