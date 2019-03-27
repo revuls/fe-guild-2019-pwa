0042d6fa7ee3da0e5af5596158bd72c34298c55b
Cesar Redondo Bueno <Cesar.Redondo.Bueno@ing.com>
2019-03-27 12:42:05 +0100

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -81,3 +81,17 @@ window.addEventListener('load', () => {
         });
     }
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', event => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    event.preventDefault();
+
+    console.log('beforeinstallprompt fired');
+
+    // Stash the event so it can be triggered later.
+    deferredPrompt = event;
+
+    return false;
+});
